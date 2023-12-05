b599714554f31ce6573a5f71767302021bbf82de
Dainis <44690511+dgrinbergs@users.noreply.github.com>
2023-12-05 13:18:05 +0000

    day 4

--- 2023/day4/part2.go
+++ 2023/day4/part2.go
@@ -1,72 +1,91 @@
 package day4
 
 import (
-	"bufio"
-	"fmt"
-	"strconv"
-	"strings"
+  "bufio"
+  "strconv"
+  "strings"
 )
 
 func Part2Solution(scanner *bufio.Scanner) int {
-	allCards := readAllCards(scanner)
-	sum := 0
+  winningGameIdsByGameId := make(map[int][]int)
+  for _, card := range readAllCards(scanner) {
+    winningGameIdsByGameId[card.id] = card.getWinningCardIds()
+  }
 
-	for _, card := range allCards {
-		sum += card.getNumberOfNestedMatches(allCards)
-	}
+  return recurse(winningGameIdsByGameId, winningGameIdsByGameId) - 1
+}
 
-	return sum
+func recurse(all, winners map[int][]int) int {
+  if len(winners) == 0 {
+    return 1
+  }
+
+  sum := 1
+  for _, winnerIds := range winners {
+    nested := make(map[int][]int)
+    for _, i := range winnerIds {
+      nested[i] = all[i]
+    }
+
+    sum += recurse(all, nested)
+  }
+
+  return sum
 }
 
 type scratchcard struct {
-	id             int
-	winningNumbers map[int]bool
-	ownNumbers     map[int]bool
+  id             int
+  winningNumbers map[int]bool
+  ownNumbers     map[int]bool
 }
 
-func (s *scratchcard) getNumberOfNestedMatches(all []*scratchcard) int {
-	matches := 0
+func (s *scratchcard) getWinningCardIds() []int {
+  matches := 0
 
-	for number := range s.ownNumbers {
-		if _, ok := s.winningNumbers[number]; ok {
-			matches++
-		}
-	}
+  for number := range s.ownNumbers {
+    if _, ok := s.winningNumbers[number]; ok {
+      matches++
+    }
+  }
 
-	for i := 0; i < matches; i++ {
-		fmt.Printf("%d+%d=%d [%d]\n", s.id, i, s.id+i, matches)
-		matches += all[s.id+i].getNumberOfNestedMatches(all)
-	}
+  if matches == 0 {
+    return []int{}
+  }
 
-	return matches
+  winningCardIds := make([]int, matches)
+  for i := 0; i < matches; i++ {
+    winningCardIds[i] = s.id + i + 1
+  }
+
+  return winningCardIds
 }
 
 func readAllCards(scanner *bufio.Scanner) []*scratchcard {
-	scratchCards := make([]*scratchcard, 0)
+  scratchCards := make([]*scratchcard, 0)
 
-	for scanner.Scan() {
-		card := scanner.Text()
+  for scanner.Scan() {
+    card := scanner.Text()
 
-		colonIndex := strings.Index(card, ":")
-		verticalBarIndex := strings.Index(card, "|")
+    colonIndex := strings.Index(card, ":")
+    verticalBarIndex := strings.Index(card, "|")
 
-		winning := make(map[int]bool)
-		for _, number := range strings.Split(card[colonIndex:verticalBarIndex], " ") {
-			if n, err := strconv.Atoi(number); err == nil {
-				winning[n] = true
-			}
-		}
+    winning := make(map[int]bool)
+    for _, number := range strings.Split(card[colonIndex:verticalBarIndex], " ") {
+      if n, err := strconv.Atoi(number); err == nil {
+        winning[n] = true
+      }
+    }
 
-		own := make(map[int]bool)
-		for _, number := range strings.Split(card[verticalBarIndex:], " ") {
-			if n, err := strconv.Atoi(number); err == nil {
-				own[n] = true
-			}
-		}
+    own := make(map[int]bool)
+    for _, number := range strings.Split(card[verticalBarIndex:], " ") {
+      if n, err := strconv.Atoi(number); err == nil {
+        own[n] = true
+      }
+    }
 
-		id, _ := strconv.Atoi(strings.TrimSpace(card[5:colonIndex]))
-		scratchCards = append(scratchCards, &scratchcard{id, winning, own})
-	}
+    id, _ := strconv.Atoi(strings.TrimSpace(card[5:colonIndex]))
+    scratchCards = append(scratchCards, &scratchcard{id, winning, own})
+  }
 
-	return scratchCards
+  return scratchCards
 }
